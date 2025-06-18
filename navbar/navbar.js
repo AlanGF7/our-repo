bfecc3a94630c2971374b798072b76560da51dff
AlanGF7 <af189107@gmail.com>
2025-06-17 21:57:04 -0600

    Segundo avance
    "Terminado" cartas, recuerdos, cualidades... Falta agregar más fotos a Galerías y Recuerdos, también la línea del tiempo de Historia

--- navbar/navbar.js
+++ navbar/navbar.js
@@ -3,5 +3,10 @@ document.addEventListener("DOMContentLoaded", () => {
       .then(res => res.text())
       .then(data => {
         document.getElementById("navbar").innerHTML = data;
+
+        const script = document.createElement("script");
+      script.src = "https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js";
+      script.crossOrigin = "anonymous";
+      document.body.appendChild(script);
       });
   });
