951574f01c414f8ba498589e513190c6711e91ee
AlanGF7 <af189107@gmail.com>
2026-02-08 00:05:15 -0600

    San Valentine 2.0
    Teóricamente, está terminado

--- sanvalentin/carta.js
+++ sanvalentin/carta.js
@@ -2,10 +2,32 @@ const envelope = document.getElementById("envelope");
 const acceptBtn = document.getElementById("acceptBtn");
 
 envelope.addEventListener("click", () => {
-  envelope.classList.add("open");
+  envelope.classList.toggle("open");
 });
 
 acceptBtn.addEventListener("click", (e) => {
-  e.stopPropagation(); // evita reabrir el sobre
-  window.location.href = "../index.html"; // 👈 tu página final
+  e.stopPropagation();
+
+  for (let i = 0; i < 20; i++) {
+    setTimeout(createHeart, i * 100);
+  }
+
+  setTimeout(() => {
+    window.location.href = "./sanvalentin.html"; // tu página final
+  }, 2500);
 });
+
+function createHeart() {
+  const heart = document.createElement("div");
+  heart.classList.add("heart");
+  heart.innerHTML = "❤️";
+
+  heart.style.left = Math.random() * 100 + "vw";
+  heart.style.animationDuration = (2 + Math.random() * 2) + "s";
+
+  document.body.appendChild(heart);
+
+  setTimeout(() => {
+    heart.remove();
+  }, 4000);
+}
